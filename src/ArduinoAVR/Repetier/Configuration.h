5539986d9d5a578e57b9b3c2c05f8aa980610041
Marc Aurel Kiefer <marcaurelkiefer@gmx.de>
2017-05-07 12:13:01 +0200

    increase feed rate for convenience

--- src/ArduinoAVR/Repetier/Configuration.h
+++ src/ArduinoAVR/Repetier/Configuration.h
@@ -1091,12 +1091,12 @@ Mega. Used only for nonlinear systems like delta or tuga. */
     */
 #define MAX_FEEDRATE_X 200
 #define MAX_FEEDRATE_Y 200
-#define MAX_FEEDRATE_Z 5
+#define MAX_FEEDRATE_Z 20
 
 /** Home position speed in mm/s. Overridden if EEPROM activated. */
 #define HOMING_FEEDRATE_X 80
 #define HOMING_FEEDRATE_Y 80
-#define HOMING_FEEDRATE_Z 3
+#define HOMING_FEEDRATE_Z 10
 
 /** Set order of axis homing. Use HOME_ORDER_XYZ and replace XYZ with your order. 
  * If you measure Z with your extruder tip you need a hot extruder to get right measurement. In this
